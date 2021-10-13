8f37e980662c5fa697232f0d12f9c37ffbd03920
Nikita-2007 <nikita2007ua@gmail.com>
2021-10-13 20:18:31 +0300

    Create 63.pas

---
+++ 63.pas
@@ -0,0 +1,18 @@
+﻿program prog05;
+ 
+var x, y, S, P:integer;
+ 
+begin
+readln(S, P);
+  for x := 1 to 1000 do
+  begin
+  if (x-1 + y = S) and ((x-1) * y = P) then
+    break;
+  for y := 1 to 1000 do
+    begin
+      if (x + y = S) and (x * y = P) then
+        break;
+    end;
+  end;
+writeln(x-1, ' ', y);
+end.
