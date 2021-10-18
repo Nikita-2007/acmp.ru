c3092b7760c5c12e0a6f1e088bc7d14192eca1cc
Nikita-2007 <nikita2007ua@gmail.com>
2021-10-18 21:33:48 +0300

    2 задачи

---
+++ 697.pas
@@ -0,0 +1,12 @@
+﻿program prog697;
+var L, W, H, x:integer;
+ 
+begin
+readln(L, W, H);
+
+x := ((L*H)+(W*H))*2;
+if x mod 16 > 0 then
+  writeln((x div 16)+1)
+else
+  write(x div 16);
+end.
---
+++ 777.pas
@@ -0,0 +1,11 @@
+﻿program prog777;
+ 
+var S,T:integer;
+ 
+begin
+read(S, T);
+if S < T then
+  write(T-S)
+else
+  write(T-S+12);
+end.
