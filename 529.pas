00aca35d5cc60983c3d9999531ccfcb6fd2c167f
Nikita-2007 <nikita2007ua@gmail.com>
2021-10-19 20:13:30 +0300

    Create 529.pas

---
+++ 529.pas
@@ -0,0 +1,11 @@
+﻿program prog529;
+ 
+var x1, x2, y1, y2, kat1, kat2, g:real;
+ 
+begin
+read(x1, y1, x2, y2);
+kat1 := abs(x1-x2);
+kat2 := abs(y1-y2);
+g := sqrt((kat1*kat1)+(kat2*kat2));
+write(g:0:5)
+end.
