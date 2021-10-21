7e7af9a549e663ae15d9665cee1045c8268ec253
Nikita-2007 <nikita2007ua@gmail.com>
2021-10-21 20:22:43 +0300

    Create 14.pas

---
+++ 14.pas
@@ -0,0 +1,15 @@
+﻿program prog14;
+ 
+var N, M, i, x:integer;
+ 
+begin
+read(N, M);
+for i := 1 to N*M do
+  begin
+  x := N*i;
+  if x mod M = 0 then
+    begin
+    writeln(x);
+    break;
+    end;
+    end;
