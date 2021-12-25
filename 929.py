1c0e29f3b06dea32a877e453e893c540eb0f383b
Nikita-2007 <nikita2007ua@gmail.com>
2021-12-25 18:17:25 +0300

    Update 929.py

--- 929.py
+++ 929.py
@@ -1,15 +1,5 @@
 N = int(input())
 a = (N - (N // 6 * 6))
-if a == 1:
-    a = 6
-elif a == 2:
-    a = 5
-elif a == 3:
-    a = 4
-elif a == 4:
-    a = 3
-elif a == 5:
-    a = 2
-elif a == 6:
-    a = 1
-print(a + (N // 6), N*6)
+if a == 0:
+    a += 7
+print(7 - a + (N // 6), N*6)
