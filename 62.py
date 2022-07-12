36f58be49cf66532f294dc2867a5ba386d8b281c
Nikita-2007 <nikita2007ua@gmail.com>
2022-07-12 20:09:27 +0300

    Update 62.py

--- 62.py
+++ 62.py
@@ -1,6 +1,2 @@
 N = input()
-x = N[0:1]
-x = True if x == 'A' or x == 'C' or x =='E' or x == 'G' else False
-if int(N[1:2]) % 2 == 0 and x: print('WHITE')
-elif int(N[1:2]) % 2 == 1 and not x: print('WHITE')
-else: print('BLACK')
+print('BLACK') if (ord(N[0:1]) + int(N[1:2])) % 2 == 0 else print('WHITE')
