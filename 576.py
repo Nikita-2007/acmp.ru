2c92c0aa25de087637b6d04440126974c908236d
Nikita-2007 <nikita2007ua@gmail.com>
2023-06-05 17:00:20 +0300

    2 задачи

--- 576.py
+++ 576.py
@@ -1,1 +1,13 @@
-
+n = int(input())
+s = 0
+for i in range(1, n):
+	a = i
+	b = n
+	while a!=0 and b!=0:
+		if a>b:
+			a=a%b
+		else:
+			b=b%a
+	if a+b == 1:
+		s += 1
+print(s)
---
+++ 706.py
@@ -0,0 +1,2 @@
+r, x, y = map(int, input().split())
+print(round(r*abs(x)/(2*r-y), 2))
