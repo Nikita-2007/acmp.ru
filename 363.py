d0eadde17b51c5daa0592d4c9961da8df9e9f7eb
Nikita <55892093+Nikita-2007@users.noreply.github.com>
2024-01-04 20:34:11 +0300

    Update 363.py

--- 363.py
+++ 363.py
@@ -1,16 +1,10 @@
 n = input()
 m = input()
-l = (len(n)+len(m))
-f = [0]*l
+f = 0
 if len(m) > len(n):
 	n, m = m, n
-arr = [[i*j for i in range(10)] for j in range(10)]
-
-for i in range(len(m)-1, -1 , -1):
-	for j in range(len(n)-1, -1, -1):
-		f[i+j+1] += arr[int(m[i])][int(n[j])]
-for i in range(l-1, -1, -1):
-	f[i-1] += f[i]//10
-	f[i] %= 10
-
-print(int(''.join(map(str, f))))
+n = int(n)
+arr = [n*i for i in range(10)]
+for i in range(len(m)-1, -1, -1):
+	f += arr[int(m[-i-1])]*10**i
+print(f)
