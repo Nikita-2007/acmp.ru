d65e1f1e8f6f73bcbdf832a9b589fe83ca04c1c9
Nikita <55892093+Nikita-2007@users.noreply.github.com>
2024-01-06 20:05:12 +0300

    Update 712.py

--- 712.py
+++ 712.py
@@ -1,10 +1,10 @@
 w, h, n = map(int, input().split())
-f = w*n
-mini = f
-for i in range(1, n):
-	x = f // i
-	if x < h*i:
-		break
-	s = (int(x**0.5)+1)**2
-	mini = min(mini, f)
-print(f)
+maxi = max(w, h)
+mini = min(w, h) * n
+while (maxi < mini):
+    mid = (maxi + mini) // 2;
+    if (n <= (mid // w) * (mid // h)):
+        mini = mid
+    else:
+        maxi = mid + 1
+print(maxi)
