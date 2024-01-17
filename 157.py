14a55a4cb1ac9dd42ada709075a2fbe3adc67703
Nikita <55892093+Nikita-2007@users.noreply.github.com>
2024-01-17 20:58:41 +0300

    2

--- 157.py
+++ 157.py
@@ -1,8 +1,6 @@
+f = [0,1,2,6,24,120,720,5040,40320,362880,3628800,39916800,479001600,6227020800,87178291200,1307674368000,20922789888000]
 s = input()
-arr = []
-f = 1
+r = f[len(s)]
 for i in set(s):
-	arr.append(s.count(i))
-for i in range(1, len(s)+1):
-	f *= i
-print(f)
+	r //= f[s.count(i)]
+print(r)
